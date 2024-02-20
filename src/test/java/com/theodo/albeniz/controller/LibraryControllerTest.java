4c0335287dd971a227fbe061d78fa5bbd8abfd2f
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-20 11:06:38 +0100

    feat: create a controller test

---
+++ src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
@@ -0,0 +1,31 @@
+package com.theodo.albeniz.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LibraryController.class)
+@AutoConfigureMockMvc
+class LibraryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetLibraryRoute() throws Exception {
+        mockMvc.perform(get("/library/music").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        "[" +
+                                "{'title':'Thriller','author':'MJ'}," +
+                                "{'title':'Prelude and Fugue in C minor','author':'Bach'}" +
+                                "]"));
+    }
+}
