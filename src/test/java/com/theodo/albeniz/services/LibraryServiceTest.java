f9d73c106c10afcb8734ec70b293c46a6d990428
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-20 14:41:39 +0100

    feat: isolate test logic

---
+++ src/test/java/com/theodo/albeniz/services/LibraryServiceTest.java
@@ -0,0 +1,40 @@
+package com.theodo.albeniz.services;
+
+import com.theodo.albeniz.dto.Tune;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryServiceTest {
+
+    private final LibraryService libraryService = new LibraryService();
+
+    @Test
+    public void testGetAllWithoutFilter(){
+        Collection<Tune> all = libraryService.getAll(null);
+        assertEquals(3, all.size());
+    }
+    @Test
+    public void testGetAllWithMatchingFilter(){
+        Collection<Tune> all = libraryService.getAll("Thrill");
+        assertEquals(1, all.size());
+    }
+    @Test
+    public void testGetAllWithNoMatchFilter(){
+        Collection<Tune> all = libraryService.getAll("ACDC");
+        assertEquals(0, all.size());
+    }
+
+    @Test
+    public void testGetOne(){
+        Tune tune = libraryService.getOne(1);
+        assertEquals("Thriller", tune.getTitle());
+    }
+    @Test
+    public void testGetOneNoMatch(){
+        Tune tune = libraryService.getOne(100);
+        assertNull(tune);
+    }
+}
