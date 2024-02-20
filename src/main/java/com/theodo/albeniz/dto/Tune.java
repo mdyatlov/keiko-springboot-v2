caf02a3a6be2898aa92977a527262c9351cd68e9
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-20 10:43:38 +0100

    feat: add lombok dependency and use it in dto

--- src/main/java/com/theodo/albeniz/dto/Tune.java
+++ src/main/java/com/theodo/albeniz/dto/Tune.java
@@ -1,27 +1,13 @@
 package com.theodo.albeniz.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class Tune {
     private String title;
     private String author;
-
-    public Tune(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
