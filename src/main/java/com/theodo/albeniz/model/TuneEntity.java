221fa4ebc436e75687cabcaa8ab17e375967e32d
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-22 16:14:29 +0100

    feat: add tune entity

---
+++ src/main/java/com/theodo/albeniz/model/TuneEntity.java
@@ -0,0 +1,29 @@
+package com.theodo.albeniz.model;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="TUNE")
+@Getter @Setter
+public class TuneEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "AUTHOR")
+    private String author;
+
+    @Column(name = "RELEASE_DATE")
+    private String releaseDate;
+}
