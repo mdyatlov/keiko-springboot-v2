95adb244fc484ed08695e62115f134374c1f7d2a
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-23 22:06:11 +0100

    feat: add e2e test

--- src/test/java/com/theodo/albeniz/controller/AuthControllerTest.java
+++ src/test/java/com/theodo/albeniz/controller/AuthControllerTest.java
@@ -1,21 +1,22 @@
 package com.theodo.albeniz.controller;
 
-import com.theodo.albeniz.model.UserEntity;
 import com.theodo.albeniz.repositories.UserEntityRepository;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -64,7 +65,7 @@ public class AuthControllerTest {
                 .andExpect(status().isUnauthorized());
     }
 
-/*    private void insertOneUser() throws Exception {
+    private void insertOneUser() throws Exception {
         mockMvc.perform(post("/users/sign-up")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"username\": \"user1@gmail.com\", \"password\": \"my12345pwd\"}"))
@@ -73,13 +74,6 @@ public class AuthControllerTest {
                         "{'username': 'user1@gmail.com'}"));
 
         assertEquals(1, userEntityRepository.count());
-    }*/
-
-    private void insertOneUser() throws Exception {
-        UserEntity entity = new UserEntity();
-        entity.setUsername("user1@gmail.com");
-        entity.setPassword(new BCryptPasswordEncoder().encode("my12345pwd"));
-        userEntityRepository.save(entity);
     }
 
 }
