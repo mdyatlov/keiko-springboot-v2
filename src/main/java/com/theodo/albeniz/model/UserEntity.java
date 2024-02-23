ce46687091baed5829df7d07107f61af5c555c81
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-23 12:20:37 +0100

    feat: create user entity

---
+++ src/main/java/com/theodo/albeniz/model/UserEntity.java
@@ -0,0 +1,60 @@
+package com.theodo.albeniz.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="APP_USER")
+@Getter
+@Setter
+public class UserEntity implements UserDetails {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "USER_NAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    @JsonIgnore
+    private String password;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+}
