3b446e03a42ff17b8b5029c3851ef2d0055e29ae
Guillaume MICHEL <guillaume.michel@ext.bpifrance.fr>
2024-02-23 18:44:11 +0100

    feat: test without mockuser

--- src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
+++ src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
@@ -1,13 +1,18 @@
 package com.theodo.albeniz.controller;
 
 import com.theodo.albeniz.config.WebSecurityConfiguration;
+import com.theodo.albeniz.model.UserEntity;
+import com.theodo.albeniz.repositories.UserEntityRepository;
 import com.theodo.albeniz.services.InMemoryLibraryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -15,6 +20,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,9 +35,15 @@ class LibraryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserEntityRepository userEntityRepository;
+
     @Test
+    @WithMockUser(username = "joe", password = "Th@t'sAGreatPassword!!!")
     public void testGetLibraryRoute() throws Exception {
-        mockMvc.perform(get("/library/music").contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/library/music")
+                        .with(httpBasic("joe","Th@t'sAGreatPassword!!!"))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(
                         """
@@ -44,8 +57,35 @@ class LibraryControllerTest {
     }
 
     @Test
+    public void testGetLibraryRouteWithoutMockuser() throws Exception {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(UUID.randomUUID());
+        userEntity.setUsername("joe");
+        userEntity.setPassword(new BCryptPasswordEncoder().encode("Th@t'sAGreatPassword!!!"));
+        when(userEntityRepository.findByUsername("joe")).thenReturn(userEntity);
+
+        mockMvc.perform(get("/library/music")
+                        .with(httpBasic("joe","Th@t'sAGreatPassword!!!"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                [
+                                    {'title':'Thriller','author':'MJ'},
+                                    {'title':'Prelude and Fugue in C minor','author':'Bach'},
+                                    {'title':'The Little Foam Man','author':'Patrick S.'}
+                                ]
+                                """
+                ));
+    }
+
+    @Test
+    @WithMockUser(username = "joe", password = "Th@t'sAGreatPassword!!!")
     public void testGetOneTune() throws Exception {
-        mockMvc.perform(get("/library/music/f1c236cb-3ee5-47e8-9034-d3ebf85a6b76").contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/library/music/f1c236cb-3ee5-47e8-9034-d3ebf85a6b76")
+                                .with(httpBasic("joe","Th@t'sAGreatPassword!!!"))
+                       .contentType(MediaType.APPLICATION_JSON)
+                        )
                 .andExpect(status().isOk())
                 .andExpect(content().json(
                         """
@@ -55,8 +95,10 @@ class LibraryControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "joe", password = "Th@t'sAGreatPassword!!!")
     public void testGetOneTuneNotExisting() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/library/music/" + UUID.randomUUID())
+                        .with(httpBasic("joe","Th@t'sAGreatPassword!!!"))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound()).andReturn();
         String contentAsString = mvcResult.getResponse().getContentAsString();
@@ -64,8 +106,10 @@ class LibraryControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "joe", password = "Th@t'sAGreatPassword!!!")
     public void findMusic() throws Exception {
         mockMvc.perform(get("/library/music?query=iller")
+                        .with(httpBasic("joe","Th@t'sAGreatPassword!!!"))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(
