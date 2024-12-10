e50748f7b16faff310a6e5b1c6dee81926958ebb
Mikhail Dyatlov <mdyatlov@protonmail.com>
2024-12-10 22:27:52 +0100

    fix(tests): added mockbean to mock the indatabase library service

--- src/test/java/com/theodo/albeniz/controller/InDatabaseLibraryControllerTest.java
+++ src/test/java/com/theodo/albeniz/controller/InDatabaseLibraryControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -35,6 +36,7 @@ class InDatabaseLibraryControllerTest {
   private MockMvc mockMvc;
 
   @Autowired
+  @MockBean
   private InDatabaseLibraryService libraryService;
 
   @BeforeEach
