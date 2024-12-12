da8ea7a43ee84da5426ed123f1bd5dd14f67f46e
Mikhail Dyatlov <mdyatlov@protonmail.com>
2024-12-12 15:52:57 +0100

    test(repository): added sql annotation fixture to get a ready database for tests

--- src/test/java/com/theodo/albeniz/repositories/TuneRepositoryTest.java
+++ src/test/java/com/theodo/albeniz/repositories/TuneRepositoryTest.java
@@ -4,12 +4,21 @@ import com.theodo.albeniz.model.TuneEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Sql(
+        statements = "INSERT INTO TUNE(ID, TITLE, AUTHOR) " +
+        "VALUES " +
+        "   ('f48ae564-9574-417d-b458-7933cc824f56', 'AABCC', '1111')," +
+        "   ('4577650d-b40d-4c72-9d39-c6916ad063c7', 'ABC', '2222')," +
+        "   ('a9900fc6-e4f1-4a66-aa17-d66fc10339c0', 'XXXXX', 1111)" +
+        ";"
+    )
 @DataJpaTest
 class TuneRepositoryTest {
 
@@ -18,54 +27,37 @@ class TuneRepositoryTest {
 
     @Test
     public void testCRUD() {
-        assertThat(tuneRepository.count()).isEqualTo(0);
-        TuneEntity save1 = tuneRepository.save(new TuneEntity(null, "b", "c", "d"));
-        TuneEntity save2 = tuneRepository.save(new TuneEntity(null, "e", "f", "g"));
-        TuneEntity save3 = tuneRepository.save(new TuneEntity(null, "h", "i", "j"));
-
-        assertThat(save1.getId()).isNotNull();
-        assertThat(save2.getId()).isNotNull();
-        assertThat(save3.getId()).isNotNull();
-
         assertThat(tuneRepository.count()).isEqualTo(3);
-        assertThat(tuneRepository.existsById(save2.getId())).isTrue();
+        TuneEntity savedTune = tuneRepository.save(new TuneEntity(null, "e", "f", "g"));
+        
+        assertThat(savedTune.getId()).isNotNull();
+        
+        assertThat(tuneRepository.count()).isEqualTo(4);
+        assertThat(tuneRepository.existsById(savedTune.getId())).isTrue();
         assertThat(tuneRepository.existsById(UUID.randomUUID())).isFalse();
 
-        tuneRepository.save(new TuneEntity(save2.getId(), "E", "F", "G"));
+        tuneRepository.save(new TuneEntity(savedTune.getId(), "E", "F", "G"));
 
-        Optional<TuneEntity> optional = tuneRepository.findById(save2.getId());
+        Optional<TuneEntity> optional = tuneRepository.findById(savedTune.getId());
         assertThat(optional.isPresent()).isTrue();
         assertThat(optional.get().getTitle()).isEqualTo("E");
 
-        tuneRepository.deleteById(save2.getId());
-        assertThat(tuneRepository.count()).isEqualTo(2);
-        assertThat(tuneRepository.existsById(save2.getId())).isFalse();
+        tuneRepository.deleteById(savedTune.getId());
+        assertThat(tuneRepository.count()).isEqualTo(3);
+        assertThat(tuneRepository.existsById(savedTune.getId())).isFalse();
     }
 
     @Test
     public void testSearchBy() {
-        tuneRepository.save(new TuneEntity(null, "a", "b", "c"));
-        tuneRepository.save(new TuneEntity(null, "d", "e", "f"));
-        tuneRepository.save(new TuneEntity(null, "g", "h", "i"));
-
-        assertThat(tuneRepository.searchBy("a", null)).hasSize(1);
-        assertThat(tuneRepository.searchBy("b", null)).isEmpty();
-        assertThat(tuneRepository.searchBy("d", null)).hasSize(1);
-        assertThat(tuneRepository.searchBy("e", null)).isEmpty();
-        assertThat(tuneRepository.searchBy("g", null)).hasSize(1);
-        assertThat(tuneRepository.searchBy("h", null)).isEmpty();
-        assertThat(tuneRepository.searchBy("z", null)).isEmpty();
+        assertThat(tuneRepository.searchBy("AABCC", null)).hasSize(1);
+        assertThat(tuneRepository.searchBy("ABC", null)).hasSize(2);
+        assertThat(tuneRepository.searchBy("ZZZ", null)).isEmpty();
     }
 
     @Test
     public void testFindByAuthor() {
-        tuneRepository.save(new TuneEntity(null, "a", "b", "c"));
-        tuneRepository.save(new TuneEntity(null, "d", "e", "f"));
-        tuneRepository.save(new TuneEntity(null, "g", "h", "i"));
-
-        assertThat(tuneRepository.findByAuthor("b")).hasSize(1);
-        assertThat(tuneRepository.findByAuthor("e")).hasSize(1);
-        assertThat(tuneRepository.findByAuthor("h")).hasSize(1);
-        assertThat(tuneRepository.findByAuthor("z")).isEmpty();
+        assertThat(tuneRepository.findByAuthor("1111")).hasSize(2);
+        assertThat(tuneRepository.findByAuthor("2222")).hasSize(1);
+        assertThat(tuneRepository.findByAuthor("3333")).isEmpty();
     }
 }
