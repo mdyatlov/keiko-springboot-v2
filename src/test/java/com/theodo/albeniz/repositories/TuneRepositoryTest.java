3c165d8b36c1c1ef91c3aef806abd4d13e0d5d70
Mikhail Dyatlov <mdyatlov@protonmail.com>
2024-12-12 14:17:29 +0100

    format(test): formatted a test file

--- src/test/java/com/theodo/albeniz/repositories/TuneRepositoryTest.java
+++ src/test/java/com/theodo/albeniz/repositories/TuneRepositoryTest.java
@@ -8,8 +8,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.Optional;
 import java.util.UUID;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
 class TuneRepositoryTest {
 
@@ -17,28 +17,28 @@ class TuneRepositoryTest {
     private TuneRepository tuneRepository;
 
     @Test
-     public void testCRUD() {
-         assertThat(tuneRepository.count()).isEqualTo(0);
-         TuneEntity save1 = tuneRepository.save(new TuneEntity(null, "b", "c", "d"));
-         TuneEntity save2 =tuneRepository.save(new TuneEntity(null, "e", "f", "g"));
-         TuneEntity save3 =tuneRepository.save(new TuneEntity(null, "h", "i", "j"));
+    public void testCRUD() {
+        assertThat(tuneRepository.count()).isEqualTo(0);
+        TuneEntity save1 = tuneRepository.save(new TuneEntity(null, "b", "c", "d"));
+        TuneEntity save2 = tuneRepository.save(new TuneEntity(null, "e", "f", "g"));
+        TuneEntity save3 = tuneRepository.save(new TuneEntity(null, "h", "i", "j"));
 
-         assertThat(save1.getId()).isNotNull();
-         assertThat(save2.getId()).isNotNull();
-         assertThat(save3.getId()).isNotNull();
+        assertThat(save1.getId()).isNotNull();
+        assertThat(save2.getId()).isNotNull();
+        assertThat(save3.getId()).isNotNull();
 
-         assertThat(tuneRepository.count()).isEqualTo(3);
-         assertThat(tuneRepository.existsById(save2.getId())).isTrue();
-         assertThat(tuneRepository.existsById(UUID.randomUUID())).isFalse();
+        assertThat(tuneRepository.count()).isEqualTo(3);
+        assertThat(tuneRepository.existsById(save2.getId())).isTrue();
+        assertThat(tuneRepository.existsById(UUID.randomUUID())).isFalse();
 
-         tuneRepository.save(new TuneEntity(save2.getId(), "E", "F", "G"));
+        tuneRepository.save(new TuneEntity(save2.getId(), "E", "F", "G"));
 
-         Optional<TuneEntity> optional = tuneRepository.findById(save2.getId());
-         assertThat(optional.isPresent()).isTrue();
-         assertThat(optional.get().getTitle()).isEqualTo("E");
+        Optional<TuneEntity> optional = tuneRepository.findById(save2.getId());
+        assertThat(optional.isPresent()).isTrue();
+        assertThat(optional.get().getTitle()).isEqualTo("E");
 
-         tuneRepository.deleteById(save2.getId());
-         assertThat(tuneRepository.count()).isEqualTo(2);
-         assertThat(tuneRepository.existsById(save2.getId())).isFalse();
-     }
+        tuneRepository.deleteById(save2.getId());
+        assertThat(tuneRepository.count()).isEqualTo(2);
+        assertThat(tuneRepository.existsById(save2.getId())).isFalse();
+    }
 }
