97dc9fddfe87e5babd5aef3e5d97f46ade2f6223
Mikhail Dyatlov <mdyatlov@protonmail.com>
2024-12-13 12:00:37 +0100

    feat(database): added user entity

---
+++ src/main/java/com/theodo/albeniz/database/entities/UserEntity.java
@@ -0,0 +1,54 @@
+package com.theodo.albeniz.database.entities;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "APP_USER")
+@Getter @Setter
+public class UserEntity implements UserDetails {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name="USER_NAME")
+  private String username;
+
+  @Column(name="PASSWORD")
+  private String password;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
